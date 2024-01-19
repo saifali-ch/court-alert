50b1c1f7d0d304fcf99a106d0ca3aff751265cd0
saif <chsaifali097@gmail.com>
2024-01-19 21:14:24 +0500

    Add alert display functionality

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+import secrets
+
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 
 from db import get_criteria, update_criteria_in_db, update_criteria_status, delete_criteria_from_db, \
     add_criteria_to_db
 
 app = Flask(__name__)
+
+app.secret_key = secrets.token_hex(16)
 
 
 @app.route('/')
@@ -22,7 +26,7 @@ def add_criteria():
     active = request.form.get('active', 0)  # default to 0 if not provided
 
     add_criteria_to_db(date, start_time, end_time, duration, active)
-
+    flash('Criteria added successfully!', 'success')
     return redirect(url_for('index'))
 
 
@@ -37,6 +41,7 @@ def update_criteria():
     active = request.form.get('active', 0)  # default to 0 if not provided
 
     update_criteria_in_db(criteria_id, date, start_time, end_time, duration, active)
+    flash('Criteria updated successfully!', 'primary')
     return redirect(url_for('index'))
 
 
@@ -55,6 +60,7 @@ def disable_criteria(criteria_id):
 @app.route('/delete_criteria/<criteria_id>')
 def delete_criteria(criteria_id):
     delete_criteria_from_db(criteria_id)
+    flash('Criteria deleted successfully!', 'danger')
     return redirect(url_for('index'))
 
 
